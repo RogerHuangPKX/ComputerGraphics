cef11412406d7db0abe10efe46d2a54ef9ea66a5
RogerHuang_CAU <791752911@qq.com>
2021-03-08 10:35:48 +0800

    Add files via upload

---
+++ 0-3.cpp
@@ -0,0 +1,57 @@
+﻿//程序名称：实验0-2：多笔绘制如图所示的金刚石图案
+//程序功能：输入点数，然后绘制金刚石
+//编译环境：Visual Studio 2019，EasyX_20210224
+//作者：黄金泽 2019308250202 2019308250202@cau.edu.cn
+//最后修改时间：1/3/2021
+
+#include <iostream>
+#include<easyx.h>
+#include<graphics.h>
+#include<conio.h>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+constexpr auto PI = 3.1415926;
+
+using namespace std;
+
+int main()
+{
+	int n;
+	std::cout << "请输入点的个数（点为奇数且N>=3):";
+	std::cin >> n;
+	if (n < 3 or n % 2 == 0)
+	{
+		std::cout << "输入错误！程序退出。";
+		getchar();
+		exit(65522);
+	}
+	initgraph(640, 640);
+	int x0 = 320; int y0 = 320; int r = 130;
+	double t = 2*PI / n;
+	vector<int> px;
+	vector<int> py;
+	setbkcolor(WHITE);
+	cleardevice();
+	setlinecolor(BLUE);
+	for (size_t i = 0; i < n; i++)
+	{
+		px.push_back(r * cos(i * t) + x0);
+		py.push_back(r * sin(i * t) + y0);
+	}
+
+
+	for (size_t i = 0; i <n; i++)
+	{
+
+		for (size_t j = 1; j < 0.5*n; j++)
+		{
+			int now_x, now_y, next_x, next_y;
+			line(px[(i + j) % n], py[(i + j) % n], px[(i + j+j) % n], py[(i + j+j) % n]);
+		}
+			
+	}
+	_getch();
+
+	return 0;
+}
---
+++ 0-5.cpp
@@ -0,0 +1,46 @@
+﻿//程序名称：实验0-5：实现鼠标画线的功能
+//程序功能：实现鼠标画线的功能
+//        按下鼠标左键确定线段起点，抬起左键确定终点并画线
+//		  按下鼠标右键清空窗口
+//编译环境：Visual Studio 2019，EasyX_20210224
+//作者：黄金泽 2019308250202 2019308250202@cau.edu.cn
+//最后修改时间：1/3/2021
+
+#include <iostream>
+#include<easyx.h>
+#include<graphics.h>
+#include<conio.h>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+constexpr auto PI = 3.1415926;
+
+using namespace std;
+
+int main()
+{
+	initgraph(640, 480);
+	MOUSEMSG m;
+	
+	setbkcolor(0x3893e6);
+	cleardevice();
+	int x, y;
+	while (1)
+	{
+		m = GetMouseMsg();
+		moveto(m.x, m.y);
+		if (m.uMsg==WM_LBUTTONDOWN)
+		{
+			x = m.x; y = m.y;
+		}
+		if (m.uMsg == WM_LBUTTONUP)
+		{
+			line(x, y, m.x, m.y);
+		}
+		if(m.uMsg==WM_RBUTTONDOWN)
+		 cleardevice();
+	}
+
+	_getch();
+	
+}
